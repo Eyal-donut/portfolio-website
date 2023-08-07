2362706d122fe280b37f0e0eacc56916f14fb8f4
Eyal-donut <eyallila2@gmail.com>
2023-08-07 22:50:07 +0200

    .

--- src/components/global components/Navbar/Navbar.tsx
+++ src/components/global components/Navbar/Navbar.tsx
@@ -1,15 +1,24 @@
 import { FC } from "react";
 import NavButton from "../NavButton/NavButton";
+import getHTMLBySectionName from "../../../utils/getHTMLBySectionName";
+import scrollToHTML from "../../../utils/scrollToHTML";
 import classes from "./Navbar.module.css";
 
 const Navbar: FC = () => {
+  
+  const handleClick = (name: "Home" | "About" | "Projects" | "Contact") => {
+    const targetHTML = getHTMLBySectionName(name);
+    if (!targetHTML) throw new Error("Error in getHTMLBySectionName");
+    scrollToHTML(targetHTML);
+  };
+
   return (
     <nav className={classes.navbar}>
       <ul className="centered-flex-container">
-        <NavButton name="Home"></NavButton>
-        <NavButton name="About"></NavButton>
-        <NavButton name="Projects"></NavButton>
-        <NavButton name="Contact"></NavButton>
+        <NavButton name="Home" onBtnClick={handleClick}></NavButton>
+        <NavButton name="About" onBtnClick={handleClick}></NavButton>
+        <NavButton name="Projects" onBtnClick={handleClick}></NavButton>
+        <NavButton name="Contact" onBtnClick={handleClick}></NavButton>
       </ul>
     </nav>
   );
