fcfff38e8b4261007e625a949d5f8e1cdfb0f115
Eyal-donut <eyallila2@gmail.com>
2023-08-10 18:20:11 +0200

    .

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,8 @@
 import { FC } from "react";
-import Main from "../../components/section specific components/Main/Main";
-import About from "../../components/section specific components/About/About";
-import Projects from "../../components/section specific components/Projects/Projects";
-import Contact from "../../components/section specific components/Contact/Contact";
+import Main from "../../components/section-specific components/Main/Main";
+import About from "../../components/section-specific components/About/About";
+import Projects from "../../components/section-specific components/Projects/Projects";
+import Contact from "../../components/section-specific components/Contact/Contact";
 
 const HomePage: FC = () => {
   return (
