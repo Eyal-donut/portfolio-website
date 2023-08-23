81d524f837ffd1cea1b1f8a27171470b526bcedd
Eyal-donut <eyallila2@gmail.com>
2023-08-23 11:06:44 +0200

    .

--- src/components/section-specific components/About/TechnologiesSubsection/TechnologiesSubsection.tsx
+++ src/components/section-specific components/About/TechnologiesSubsection/TechnologiesSubsection.tsx
@@ -1,11 +1,5 @@
 import { FC } from "react";
-import {
-  backendAndDatabaseTechnologies,
-  frontendTechnologies,
-  programmingLangs,
-  testingScrapingAndAPI,
-  versionControlTechnologies,
-} from "../../../../constants/constants";
+import { technologiesInfo } from "../../../../constants/constants";
 import TechnologiesGroup from "../TechnologiesGroup/TechnologiesGroup";
 import ExpandableSection from "../../../global components/ExpandableSection/ExpandableSection";
 
@@ -19,26 +13,15 @@ const TechnologiesSubsection: FC = () => {
         at? Beatae commodi eius voluptates, modi ullam atque aut sunt!
       </p>
       <ExpandableSection expandedMaxHeight={1000} collapsedMaxHeight={25}>
-        <TechnologiesGroup
-          header="programming languages"
-          technologies={programmingLangs}
-        />
-        <TechnologiesGroup
-          header="front-end"
-          technologies={frontendTechnologies}
-        />
-        <TechnologiesGroup
-          header="back-end & database"
-          technologies={backendAndDatabaseTechnologies}
-        />
-        <TechnologiesGroup
-          header="version control"
-          technologies={versionControlTechnologies}
-        />
-        <TechnologiesGroup
-          header="testing, web-scraping and api"
-          technologies={testingScrapingAndAPI}
-        />
+        {technologiesInfo.map((tech) => {
+          return (
+            <TechnologiesGroup
+              key={tech.header}
+              header={tech.header}
+              technologies={tech.technologies}
+            />
+          );
+        })}
       </ExpandableSection>
     </>
   );
