8fe1f2d010236f930628e3344969fe992ff3b5ba
Eyal-donut <eyallila2@gmail.com>
2023-08-30 18:22:29 +0200

    .

--- src/components/global components/HamburgerMenu/Menu/Menu.tsx
+++ src/components/global components/HamburgerMenu/Menu/Menu.tsx
@@ -1,17 +1,27 @@
 import { FC } from "react";
 import classes from "./Menu.module.css";
+import MenuButton from "../MenuButton/MenuButton";
+import scrollToHTMLBySectionName from "../../../../utils/scrollToHTMLBySectionName";
 
 interface MenuProps {
   isMenuVisible: boolean;
+  
 }
 
 const Menu: FC<MenuProps> = ({ isMenuVisible }) => {
+
+  const handleClick = (name: "Home" | "About" | "Projects" | "Contact") => {
+    scrollToHTMLBySectionName(name);
+  };
+  
   return (
     <div
       className={
         isMenuVisible ? `${classes.menu} ${classes.menuVisible}` : classes.menu
       }
-    ></div>
+    >
+      <MenuButton name="Home" onBtnClick={handleClick} />
+    </div>
   );
 };
 
