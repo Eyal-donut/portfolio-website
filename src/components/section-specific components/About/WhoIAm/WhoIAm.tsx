016ca46491d95ce33380e0f4c4f243ed6ceb5e4d
Eyal-donut <eyallila2@gmail.com>
2023-09-21 12:55:10 +0200

    .

--- src/components/section-specific components/About/WhoIAm/WhoIAm.tsx
+++ src/components/section-specific components/About/WhoIAm/WhoIAm.tsx
@@ -6,13 +6,12 @@ const WhoIAm: FC = () => {
       <h4>who i am</h4>
       <p className="main-text">
         A passionate Full Stack Developer, with production-level work experience
-        and a flair to collaborate effectively within teams. My professional
-        journey extends beyond my programming skills, encompassing a diverse
-        background that includes a solid foundation in biochemical research,
-        entrepreneurial experience as a business owner, and analytical acumen
-        honed during my military service as an aerial imagery analyst. Just like
-        I learned German on my own and moved from Israel to Austria, when I set
-        my mind to something, I make it happen.
+        and a flair to collaborate within teams. My professional journey extends
+        beyond my programming skills, encompassing a diverse background that
+        includes biochemical research in breast cancer, entrepreneurial experience as a business
+        owner, and analytical acumen honed during my military service in the Israeli intelligence unit. Just like I learned German on my own, moved from
+        Israel to Austria and got my degree in German, when I set my mind to
+        something, I make it happen.
       </p>
     </>
   );
