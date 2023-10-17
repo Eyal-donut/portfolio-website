1a90ef17e07c1d1c1a332444c6f59b04864fa806
Eyal-donut <eyallila2@gmail.com>
2023-10-17 09:15:19 +0200

    ,

--- src/components/section-specific components/About/WhoIAm/WhoIAm.tsx
+++ src/components/section-specific components/About/WhoIAm/WhoIAm.tsx
@@ -8,9 +8,10 @@ const WhoIAm: FC = () => {
         A passionate Full Stack Developer, with production-level work experience
         and a flair to collaborate within teams. My professional journey extends
         beyond my programming skills, encompassing a diverse background that
-        includes biochemical research in breast cancer, entrepreneurial experience as a business
-        owner, and analytical acumen honed during my military service in the Israeli intelligence unit. Just like I learned German on my own, moved from
-        Israel to Austria and got my degree in German, when I set my mind to
+        includes biochemical research in breast cancer, entrepreneurial
+        experience, and analytical acumen honed during my military service in
+        the Israeli intelligence unit. Just like I learned German on my own,
+        moved to Austria and got my chemistry degree in German, when I set my mind to
         something, I make it happen.
       </p>
     </>
