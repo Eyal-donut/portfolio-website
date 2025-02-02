441be79f537deb59c02ca22089f7464ceba722d6
Eyal-donut <eyallila2@gmail.com>
2025-02-02 17:10:33 +0100

    .

--- src/components/pages/HomePage/About/WhoIAm/WhoIAm.tsx
+++ src/components/pages/HomePage/About/WhoIAm/WhoIAm.tsx
@@ -5,13 +5,12 @@ const WhoIAm: FC = () => {
     <>
       <h4>who i am</h4>
       <p className="main-text">
-        A passionate Full Stack Developer, with production-level work experience
-        and a flair to collaborate within teams. My professional journey extends
+      A passionate Full Stack Developer with experience in medical software, bringing both a creative and analytical approach to problem-solving. My professional journey extends
         beyond my programming skills, encompassing a diverse background that
         includes biochemical research in breast cancer, entrepreneurial
         experience, and analytical acumen honed during my military service in
-        the Israeli intelligence unit. Just like I learned German on my own,
-        moved to Austria and got my chemistry degree in German, when I set my mind to
+        the Israeli intelligence unit. Just like I moved to Austria and got my chemistry degree in German,
+        founded a business and became a software developer - each a unique challenge - whenever I set my mind to
         something, I make it happen.
       </p>
     </>
