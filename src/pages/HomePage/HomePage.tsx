76097318f48038d2f0b081f6baa87963a1617e36
Eyal-donut <eyallila2@gmail.com>
2025-02-02 14:44:54 +0100

    fixed imports

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,10 @@
 
 import { FC } from "react";
 import ScrollTrigger from "../../components/global components/ScrollTrigger/ScrollTrigger";
-import About from "../../components/section-specific components/About/About";
-import Contact from "../../components/section-specific components/Contact/Contact";
-import Main from "../../components/section-specific components/Main/Main";
-import Projects from "../../components/section-specific components/Projects/Projects";
+import About from "../../components/pages/HomePage/About/About";
+import Contact from "../../components/pages/HomePage/Contact/Contact";
+import Main from "../../components/pages/HomePage/Main/Main";
+import Projects from "../../components/pages/HomePage/Projects/Projects";
 import { projectsInfos } from "../../constants/constants";
 
 const HomePage: FC = () => {
